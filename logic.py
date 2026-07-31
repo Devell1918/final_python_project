2706e072d265318634b03b0207339a1b10a8193a
Devell1918 <devell1918@gmail.com>
2026-07-30 20:51:41 -0500

    Working as intended, fixed bug by making sure the data_frame is refreshed

--- logic.py
+++ logic.py
@@ -1,6 +1,5 @@
 import PyQt6.QtWidgets as pyqt
 import gui
-import csv
 from datetime import date
 import pandas as pd
 
@@ -9,8 +8,11 @@ class Logic(pyqt.QMainWindow, gui.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        self.current_stat = "Phone"
+        #default values
         self.label_message.setText("")
         self.dateEdit.setDate(date(2026, 1, 1))
+
         # when submit is clicked it calls sumit function
         self.button_submitEntry.clicked.connect(lambda: self.submit())
 
@@ -22,28 +24,33 @@ class Logic(pyqt.QMainWindow, gui.Ui_MainWindow):
         This method checks the data and then stores it into a csv
         """
         try:
+            #get date and phone time
             qt_date = self.dateEdit.date()
             python_date = qt_date.toPyDate()
             phone_hours = int(self.input_phone_hours.text())
             phone_minutes = int(self.input_phone_minutes.text())
             total_phone_minutes = phone_minutes + (phone_hours * 60)
-
+            #TODO seperate date and time try blocks
             #check input
+            data_frame = pd.read_csv("personal_stats_data.csv", parse_dates=["Date"])
+            if (data_frame["Date"].dt.date == python_date).any():
+                raise Exception("invalid date (duplicate)")
             if python_date < date(2026, 1, 1) or python_date > date.today():
                 raise Exception("invalid date")
             if phone_hours < 0 or phone_hours > 23:
                 raise Exception("invalid phone hours")
             if phone_minutes < 0 or phone_minutes >59:
                 raise Exception("invalid phone minutes")
-        except ValueError:
+        except ValueError as e:
             self.label_message.setText("Enter valid phone time")
+            print(e)
             return
         except Exception as e:
             print(e)
             self.label_message.setText("Enter valid phone time")
             return
 
-
+        #get selections
         selected_caffeine = self.button_group_caffeine.checkedButton()
         selected_workout = self.button_group_workout.checkedButton()
         selected_read = self.button_group_read.checkedButton()
@@ -51,15 +58,15 @@ class Logic(pyqt.QMainWindow, gui.Ui_MainWindow):
         selected_nap = self.button_group_nap.checkedButton()
         selected_wellbeing = self.button_group_wellbeing.checkedButton()
 
+        #I know I should have used a list, but this works okay because I don't have a ton of values
         if selected_caffeine and selected_workout and selected_read and selected_gamedev and selected_nap and selected_wellbeing:
             caffeine_value, workout_value, read_value, gamedev_value, nap_value, wellbeing_value = self.get_values(selected_caffeine, selected_workout, selected_read, selected_gamedev, selected_nap, selected_wellbeing)
             print(python_date, phone_hours, phone_minutes, caffeine_value, workout_value, read_value, gamedev_value, nap_value, wellbeing_value)
             self.write_values(python_date, total_phone_minutes, caffeine_value, workout_value, read_value, gamedev_value, nap_value, wellbeing_value)
-            #TODO write logic/ submission entered to message box
         else:
             print("Make sure each topic has a selection")
             self.label_message.setText("Make sure each topic has a selection")
-            #TODO display message to user
+            
         
 
 
@@ -68,7 +75,29 @@ class Logic(pyqt.QMainWindow, gui.Ui_MainWindow):
         """
         this method runs when the dropdown is changed, and displays the correct data
         """
-        pass
+        data_frame = pd.read_csv("personal_stats_data.csv", parse_dates=["Date"])
+        if self.combo_box.currentIndex() == 0:
+            print("Phone Time Selected")
+            self.calculate_and_set_averages("Phone", data_frame)
+        if self.combo_box.currentIndex() == 1:
+            print("Caffeine Selected")    
+            self.calculate_and_set_averages("Caffeine", data_frame)    
+        if self.combo_box.currentIndex() == 2:
+            print("Workout Selected")
+            self.calculate_and_set_averages("Workout", data_frame)  
+        if self.combo_box.currentIndex() == 3:
+            print("Read Selected")
+            self.calculate_and_set_averages("Read", data_frame)  
+        if self.combo_box.currentIndex() == 4:
+            print("Game Dev Selected")
+            self.calculate_and_set_averages("GameDev", data_frame)  
+        if self.combo_box.currentIndex() == 5:
+            print("Nap Selected")      
+            self.calculate_and_set_averages("Nap", data_frame)    
+        if self.combo_box.currentIndex() == 6:
+            print("Wellbeing Selected")
+            self.calculate_and_set_averages("Wellbeing", data_frame)  
+
 
     def get_values(self, selected_caffeine: int, selected_workout: int, selected_read: int, selected_gamedev: int, selected_nap: int, selected_wellbeing: int):
         """
@@ -137,9 +166,13 @@ class Logic(pyqt.QMainWindow, gui.Ui_MainWindow):
             case self.radio_wellbeing_10:
                 wellbeing_value = 10
         return caffeine_value, workout_value, read_value, gamedev_value, nap_value, wellbeing_value
+    
     def write_values(self, date: date, total_phone_minutes: int, caffeine_value: int, workout_value: int, read_value: int, gamedev_value: int, nap_value: int, wellbeing_value: int):
+        """
+        takes values and writes them to a csv file
+        """
         new_data_frame = pd.DataFrame([{"Date": date, "Phone": 0, "Caffeine": caffeine_value, "Workout": workout_value, "Read": read_value, "GameDev": gamedev_value, "Nap": nap_value, "Wellbeing": wellbeing_value}])
-        data_frame = pd.read_csv("personal_stats_data.csv")
+        data_frame = pd.read_csv("personal_stats_data.csv", parse_dates=["Date"])
 
 
         data_frame.at[data_frame.index[-1], 'Phone'] = total_phone_minutes
@@ -159,3 +192,45 @@ class Logic(pyqt.QMainWindow, gui.Ui_MainWindow):
         #             print (row)
                 
         self.label_message.setText("Submission Entered")
+        data_frame
+        #refresh averages
+        data_frame = pd.read_csv("personal_stats_data.csv", parse_dates=["Date"])
+        self.calculate_and_set_averages(self.current_stat, data_frame)
+
+    def calculate_and_set_averages(self, column: str, data_frame):
+            """
+            calculates the averages and then sets the labels accordingly
+
+            :param column: the column to be averaged
+            :param data_frame: the csv data
+            """
+
+            if column == "Phone":
+                week_av = data_frame[column].iloc[-7:-1].mean()
+                rounded_week_av = f'{week_av: .2f}'
+                self.label_weekly_output.setText(rounded_week_av)
+
+                month_av = data_frame[column].iloc[-31:-1].mean()
+                rounded_month_av = f'{month_av: .2f}'
+                self.label_monthly_output.setText(rounded_month_av)
+
+                start_av = data_frame[column].iloc[0:-1].mean()
+                rounded_start_av = f'{start_av: .2f}'
+                self.label_start_ouput.setText(rounded_start_av)
+            else:
+                week_av = data_frame[column].iloc[-6:].mean()
+                rounded_week_av = f'{week_av: .2f}'
+                self.label_weekly_output.setText(rounded_week_av)
+
+                month_av = data_frame[column].iloc[-30:].mean()
+                rounded_month_av = f'{month_av: .2f}'
+                self.label_monthly_output.setText(rounded_month_av)
+
+                start_av = data_frame[column].iloc[0:].mean()
+                rounded_start_av = f'{start_av: .2f}'
+                self.label_start_ouput.setText(rounded_start_av)
+
+            #value used to refresh values when submit is pressed
+            self.current_stat = column
+
+
